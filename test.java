76e4840880989a0ab9ff7ecc8ca4094ce83b467e
yeroiev <eroev.anton@gmail.com>
2017-03-19 11:44:40 +0200

    create test

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+	public static void main (String[]args){
+		double a=Integer.valueOf(args[0]);
+		double b=Integer.valueOf(args[1]);
+		double c=Integer.valueOf(args[2]);
+		
+		double summ=a+b+c;
+		System.out.println("summa " +summ);
+		
+	}
+}
